04e5c708d608d099465db40a0b310f67ec31d546
Deny Prasetyo <deny.prasetyo@gdplabs.id>
2015-06-19 10:39:35 +0700

    Skeleton Initial Commit

---
+++ src/test/java/id/gdplabs/olympia/pts/PtsApplicationTests.java
@@ -0,0 +1,18 @@
+package id.gdplabs.olympia.pts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PtsApplication.class)
+@WebAppConfiguration
+public class PtsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
