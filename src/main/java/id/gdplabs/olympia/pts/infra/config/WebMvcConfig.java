103272a2a017fb43f2dda0f29fc4005986406ef4
Deny Prasetyo <deny.prasetyo@gdplabs.id>
2015-06-22 12:42:05 +0700

    Add samples for Security Support

---
+++ src/main/java/id/gdplabs/olympia/pts/infra/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package id.gdplabs.olympia.pts.infra.config;
+
+/**
+ * Created by Deny Prasetyo
+ * 22 June 2015
+ * Principal Software Development Engineer
+ * GDP Labs
+ * deny.prasetyo@gdplabs.id
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/access").setViewName("access");
+        registry.addViewController("/").setViewName("home");
+    }
+
+}
