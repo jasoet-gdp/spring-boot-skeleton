6416a17a9777593433e50217f5a4187f10a79d49
Deny Prasetyo <deny.prasetyo@gdplabs.id>
2015-06-22 16:16:39 +0700

    Add VocherRepository using Spring-data-jpa

---
+++ src/main/java/id/gdplabs/olympia/pts/repository/VoucherRepository.java
@@ -0,0 +1,17 @@
+package id.gdplabs.olympia.pts.repository;
+
+import id.gdplabs.olympia.pts.entity.Voucher;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * Created by Deny Prasetyo
+ * 22 June 2015
+ * Principal Software Development Engineer
+ * GDP Labs
+ * deny.prasetyo@gdplabs.id
+ */
+
+
+public interface VoucherRepository extends PagingAndSortingRepository<Voucher,Long> {
+
+}
