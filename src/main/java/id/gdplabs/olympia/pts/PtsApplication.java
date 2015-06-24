0c0af1e426cc77806a5d32f2eb92a9e53f3d2c29
Deny Prasetyo <deny.prasetyo@gdplabs.id>
2015-06-24 16:31:37 +0700

    Invoke Create From Voucher Service

--- src/main/java/id/gdplabs/olympia/pts/PtsApplication.java
+++ src/main/java/id/gdplabs/olympia/pts/PtsApplication.java
@@ -1,13 +1,20 @@
 package id.gdplabs.olympia.pts;
 
+import id.gdplabs.olympia.pts.service.VoucherService;
+import id.gdplabs.olympia.pts.value.DiscountType;
+import id.gdplabs.olympia.pts.value.TransactionType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class PtsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PtsApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(PtsApplication.class, args);
+        ctx.getBean(VoucherService.class).create("PROMO007", 10, TransactionType.ALL, DiscountType.PERCENTAGE, 20, LocalDate.now().plusDays(35), LocalDate.now().plusDays(45));
     }
 
 }
